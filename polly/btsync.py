20d0ef3c38d1b002e5fb514a5d4f199be4f0bbce
Blake VandeMerwe <blakev@null.net>
2014-06-28 13:26:13 -0600

    added more to btsync.py connector

--- polly/btsync.py
+++ polly/btsync.py
@@ -5,7 +5,13 @@ import os
 import requests
 import simplejson as json
 
-from collections import namedtuple
+immutable_dict = frozenset(dict().items())
+
+default_response = {
+    'error': {
+        'msg': 'Could not connect to btsync'
+    }
+}
 
 class BtsyncApi(object):
     def __init__(self, btsync_conf = None, **kwargs):
@@ -16,78 +22,100 @@ class BtsyncApi(object):
                 self.config = {}
         else:
             self.config = json.load(open(btsync_conf, 'r+b'))
-
         webui = self.config.get('webui', {})
         self.address, self.port = webui.get('listen', '').split(':')
         self.login = webui.get('login', '')
         self.password = webui.get('password', '')
-
         self.bturl = 'http://{address}:{port}/api?method='.format(address = self.address, port = self.port)
-
-        if self.login + self.password == '':
-            self.pre = self.__without_auth()
-        else:
-            self.pre = self.__with_auth()
-
+        self.get = self.__request()
 
     @property
     def connected(self):
         try:
-            return self.pre(self.bturl + 'get_os', dict()).status_code == requests.codes.ok
+            resp = self.get(self.bturl + 'get_os')
+            if isinstance(resp, dict):
+                return False
+            else:
+                return resp.status_code == requests.codes.ok
         except requests.exceptions.ConnectionError as e:
             return False
 
-    def __with_auth(self):
-        return lambda url, payload: requests.get(url, auth=(self.login, self.password), params=payload)
-
-    def __without_auth(self):
-        return lambda url, payload: requests.get(url, params=payload)
+    def __request(self):
+        def inner(url, payload = None):
+            if self.login + self.password == '':
+                auth = None
+            else:
+                auth = (self.login, self.password,)
+            try:
+                return requests.get(url, auth=auth, params=payload).json()
+            except requests.exceptions.ConnectionError as e:
+                return dict(error = dict(msg = e))
+        return inner
 
     def get_folders(self, secret = None):
-        pass
+        send = {'secret': secret} if secret else None
+        return self.get(self.bturl + 'get_folders', send)
 
-    def add_folder(self, directory, secret = None, selective_sync = None):
-        pass
+    def add_folder(self, directory, secret = None, selective_sync = 0):
+        send = {'dir': directory}
+        if secret: send['secret'] = secret
+        if selective_sync: send['selective_sync'] = selective_sync
+        return self.get(self.bturl + 'add_folder', send)
 
-    def remove_folder(self, secret, path = None):
-        pass
+    def remove_folder(self, secret):
+        return self.get(self.bturl + 'remove_folder', {'secret': secret})
 
-    def set_file_preferences(self, secret, path, download):
-        pass
+    def set_file_preferences(self, secret, path, download = True):
+        send = {'secret': secret, 'path': path, 'download': 1 if download else 0}
+        return self.get(self.bturl + 'set_file_prefs', send)
+
+    def get_files(self, secret, path = None):
+        send = {'secret': secret}
+        if path: send['path'] = path
+        return self.get(self.bturl + 'get_files', send)
 
     def get_folder_peers(self, secret):
-        pass
+        return self.get(self.bturl + 'get_folder_peers', {'secret': secret})
 
-    def get_secrets(self, secret = None, type = None):
-        pass
+    def get_secrets(self, secret = None, encryption = False):
+        send = {}
+        if secret: send['secret'] = secret
+        if encryption: send['encryption'] = 'encryption'
+        return self.get(self.bturl + 'get_secrets', send)
 
     def get_folder_preferences(self, secret):
-        pass
+        return self.get(self.bturl + 'get_folder_prefs', {'secret': secret})
 
-    def set_folder_preferences(self, secret, params = dict()):
-        pass
+    def set_folder_preferences(self, secret, params = immutable_dict):
+        return self.get(self.bturl + 'set_folder_prefs', dict({'secret': secret}.items() + params.items()))
 
     def get_folder_hosts(self, secret):
-        pass
+        return self.get(self.bturl + 'get_folder_hosts', {'secret': secret}) 
 
     def set_folder_hosts(self, secret, hosts = list()):
-        pass
+        return self.get(self.bturl + 'set_folder_hosts', {'secret': secret, 'hosts': ','.join([a + ':' + b for a, b in hosts])})
 
-    def set_preferences(self, preferences = dict()):
-        pass
+    def get_preferences(self):
+        return self.get(self.bturl + 'get_prefs')
+
+    def set_preferences(self, preferences = immutable_dict):
+        return self.get(self.bturl + 'set_prefs', preferences) 
 
     def get_os(self):
-        pass
+        return self.get(self.bturl + 'get_os')
 
     def get_version(self):
-        pass
+        return self.get(self.bturl + 'get_version')
 
     def get_speed(self):
-        pass
+        return self.get(self.bturl + 'get_speed')
 
     def shutdown(self):
-        pass
+        msg = self.get(self.bturl + 'shutdown')
+        return True if msg['error'] == 0 else msg
 
 
 sync = BtsyncApi(r'../btsync.conf')
 print sync.connected
+print sync.get_version()
+print sync.get_secrets()
