1ceb57878fbf49b33eca6391f80c68f28775e8cf
Blake VandeMerwe <blakev@null.net>
2014-06-28 21:38:22 -0600

    cleaned up the api routes

--- polly/routes/api.py
+++ polly/routes/api.py
@@ -1,13 +1,28 @@
 __author__ = 'Blake VandeMerwe'
 
+import os
 from bottle import Bottle
+from btsync import BtsyncApi
+from utils import conf
 
+# define new api app
 api = Bottle()
+# get the cwd and find the configuration file used in server.py
+cwd = os.getcwd()
+config_file = os.path.join(cwd, 'polly.conf')
+# load the same config values into this api, as it's just an extension
+api.config.load_config(config_file)
+# prime utils.conf with the current app we're using
+conf = conf(api)
+# get the btsync configuration path
+btsync = BtsyncApi(conf('polly.btsync_conf_path'))
 
-@api.route('/') # supplies api commands
-def api_listing():
-    return {'.'.join(x.rule.split('/')):x.rule for x in api.routes}
-
-@api.route('/get_dirs')
+@api.route('/btsync_stats')
 def api_get_dirs():
-    return 'directories'
+    return {
+        'btsync': {
+            'version': btsync.get_version()['version'],
+            'speed': btsync.get_speed(),
+            'os': btsync.get_os()['os']
+        }
+    }
