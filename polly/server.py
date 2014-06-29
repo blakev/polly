2f4c4208e921b3736ded3965a2da3c5c3c6b6e0c
Blake VandeMerwe <blakev@null.net>
2014-06-28 21:38:07 -0600

    switched server from WSGI to SocketIO

--- polly/server.py
+++ polly/server.py
@@ -1,16 +1,25 @@
 __author__ = 'Blake VandeMerwe'
 
-import os
-import sys
 import functools
 
+import os
 import simplejson as json
-from gevent import monkey; monkey.patch_all()
-from bottle import Bottle, run, static_file
-from bottle import jinja2_view, jinja2_template as template
+
+from gevent import monkey
+monkey.patch_all()
+
+import gevent
+from socketio.server import SocketIOServer
+from socketio import socketio_manage
+
+from bottle import Bottle, static_file
+from bottle import jinja2_view
+from bottle import request
 from bottle_sqlite import SQLitePlugin
 
 from routes.api import api as pollyApi
+from btsync import BtsyncApi
+from pollyo import StatusNamespace
 import utils
 
 app = Bottle()
@@ -20,12 +29,13 @@ config_file = os.path.join(cwd, 'polly.conf')
 
 app.config.load_config(config_file)
 
-conf = lambda x, y: app.config.get(x, y)
+conf = utils.conf(app)
 view = functools.partial(jinja2_view, template_lookup=[conf('polly.template_dir', './templates')])
 
 check_files = [
     'polly.btsync_path',
     'polly.btsync_conf_path',
+    'polly.template_dir', 'polly.statics_dir',
     'sqlite.db'
 ]
 
@@ -33,28 +43,37 @@ if not os.path.exists(config_file):
     raise EnvironmentError('Could not find polly.conf')
 
 for f in check_files:
-    if not os.path.exists(conf(f, '')): raise EnvironmentError('Could not find {0}'.format(f))
+    if not os.path.exists(conf(f, '')): raise EnvironmentError('Could not find {0}: {1}'.format(f, conf(f, '')))
 
 btsync_conf = json.load(open(conf('polly.btsync_conf_path', ''), 'r+b'))
+btsync = BtsyncApi(conf('polly.btsync_conf_path', ''))
+
+app.debug = conf('debug', False)
 app.install(SQLitePlugin(dbfile=conf('sqlite.db', ':memory:')))
 
 @app.route('/')
 @view('default.html')
 def index():
-    return {'path': conf('polly.template_dir', None)}
+    return {}
 
 @app.route('/static/<filename:path>', name='static')
 def serve_static(filename):
     return static_file(filename, root=os.path.join(cwd, conf('polly.statics_dir', './static')))
 
+@app.route('/socket.io/<arg:path>', method=['OPTIONS', 'GET'])
+def socketio(*args, **kwargs):
+    path = request.environ['PATH_INFO'].strip('/')
+    if path.startswith('socket.io'):
+        socketio_manage(request.environ, {'/status': StatusNamespace}, request)
+    else:
+        return {'error': 'socket-io endpoint not found'}
+
 if __name__ == '__main__':
     app.mount('/api/', pollyApi)
-    if utils.force_btsync_restart(conf):
-        run(app,
-            debug = conf('debug', True),
-            host = conf('host', 'localhost'),
-            port = conf('port', 8080),
-            server = conf('server', 'wsgiref'),
-            reloader = conf('reloader', conf('debug', True)))
-    else:
-        sys.exit(1)
+
+    http_server = SocketIOServer(
+        (conf('host', 'localhost'), int(conf('port', 8085))), app, resource='socket.io')
+
+    gevent.joinall([
+        gevent.spawn(http_server.serve_forever)
+    ])
